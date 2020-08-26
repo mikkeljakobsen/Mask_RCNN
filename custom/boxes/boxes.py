098ee53acac2a811ad7b0fb7edd74c8bf2f42b5d
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-08-26 10:57:32 +0200

    Added train script for custom box dataset

---
+++ custom/boxes/boxes.py
@@ -0,0 +1,242 @@
+"""
+Mask R-CNN
+Train on a custom dataset with 2 classes (boxes and background)
+
+Copyright (c) 2018 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Mikkel Borring Jakobsen
+
+------------------------------------------------------------
+
+Usage: run from the command line:
+
+    # Train a new model starting from pre-trained COCO weights
+    python3 boxes.py train --dataset=/path/to/boxes/dataset --weights=coco
+
+    # Resume training a model that you had trained earlier
+    python3 boxes.py train --dataset=/path/to/boxes/dataset --weights=last
+
+    # Train a new model starting from ImageNet weights
+    python3 boxes.py train --dataset=/path/to/boxes/dataset --weights=imagenet
+
+    # Apply color splash to an image
+    python3 boxes.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
+"""
+
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+from pathlib import Path
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+############################################################
+#  Configurations
+############################################################
+
+
+class BoxConfig(Config):
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "box"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + box
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+
+############################################################
+#  Dataset
+############################################################
+
+class BoxDataset(utils.Dataset):
+
+    def load_box(self, dataset_dir, subset):
+        """Load a subset of the Box dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("box", 1, "box")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        image_paths = list(Path(dataset_dir).glob('*.jpg'))
+
+        for i, image_path in enumerate(image_paths):
+            self.add_image("box", image_id=i,
+                           path=image_path)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        info = self.image_info[image_id]
+        mask = skimage.io.imread(str(
+            Path(info['path']).parent.parent / 'mask' / '{:04}.png'.format(info['id'])
+        ))
+
+        masks = []
+        class_ids = []
+
+        for class_id in range(1, BoxConfig.NUM_CLASSES):
+            m = mask == class_id
+            area = m.sum()
+            if area > 20 ** 2: # only include object if mask-area > 20^2 pixels
+                masks.append(m)
+                class_ids.append(class_id)
+
+        masks = np.stack(masks, axis=-1)
+
+        return masks, np.array(class_ids, dtype=np.int32)
+
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "box":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+
+def train(model):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = BoxDataset()
+    dataset_train.load_box(args.dataset, "train")
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = BoxDataset()
+    dataset_val.load_box(args.dataset, "val")
+    dataset_val.prepare()
+
+    # *** This training schedule is an example. Update to your needs ***
+    # Since we're using a very small dataset, and starting from
+    # COCO trained weights, we don't need to train too long. Also,
+    # no need to train all layers, just the heads should do it.
+    print("Training network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=30,
+                layers='heads')
+
+
+############################################################
+#  Training
+############################################################
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect boxes.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/box/dataset/",
+                        help='Directory of the Box dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    args = parser.parse_args()
+
+    # Validate arguments
+    if args.command == "train":
+        assert args.dataset, "Argument --dataset is required for training"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = BoxConfig()
+    else:
+        class InferenceConfig(BoxConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        train(model)
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train'".format(args.command))
